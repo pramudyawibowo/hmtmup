ab90940ebf8c5b27c3530337858ad50d124b7fe8
rayhanrndy <rayhanrendy17@gmail.com>
2023-06-15 19:05:11 +0700

    update slicing

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,45 +1,63 @@
     @extends('guest.layout.app')
     @section('content')
         <!-- Home -->
-        <div style="background-image: url({{ asset('assets/BG.svg') }} )">
-            <h1 class="text-7xl font-bold py-4 text-neutral-50 text-center">
-                HMTM "SPRUDA"
-            </h1>
-            <div class="carousel overflow-y-hidden flex mx-32 h-96">
-                <div id="slide1" class="carousel-item relative w-full">
-                    <img src="https://universitaspertamina.ac.id/common/images/logo-decorated.png" class="w-full" />
-                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide2" class="btn btn-circle">❮</a>
-                        <a href="#slide2" class="btn btn-circle">❯</a>
-                    </div>
-                </div>
-                <div id="slide2" class="carousel-item relative w-full">
-                    <img src="https://universitaspertamina.ac.id/common/images/logo-decorated.png" class="w-full" />
-                    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide1" class="btn btn-circle">❮</a>
-                        <a href="#slide1" class="btn btn-circle">❯</a>
-                    </div>
-                </div>
+    <div style="background-image: url(/assets/BG.svg)">
+        <div class="py-8">
+          <h1
+            class="font-monserrat font-bold text-neutral-50 text-center lg:text-7xl xs:text-4xl"
+          >
+            HMTM "SPRUDA"
+          </h1>
+          <div class="carousel overflow-y-hidden mx-auto w-auto py-8">
+            <div id="slide1" class="carousel-item relative w-full">
+              <img
+                src="https://universitaspertamina.ac.id/common/images/logo-decorated.png"
+                class="w-full"
+              />
+              <div
+                class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+              >
+                <a href="#slide2" class="btn btn-circle">❮</a>
+                <a href="#slide2" class="btn btn-circle">❯</a>
+              </div>
             </div>
-            <p class="text-center text-4xl w-auto mx-48">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo,
-                magni!
-            </p>
+            <div id="slide2" class="carousel-item relative w-full">
+              <img
+                src="https://universitaspertamina.ac.id/common/images/logo-decorated.png"
+                class="w-full"
+              />
+              <div
+                class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+              >
+                <a href="#slide1" class="btn btn-circle">❮</a>
+                <a href="#slide1" class="btn btn-circle">❯</a>
+              </div>
+            </div>
+          </div>
+          <p class="text-center text-lg w-auto mx-auto">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo,
+            magni!
+          </p>
         </div>
-        <!-- Home -->
+      </div>
+      <!-- Home -->
         @include('guest.activity')
         @include('guest.library')
         @include('guest.services')
         <!-- Protect IPW -->
-        <div class="m-24">
-            <button class="btn bg-pudar border-none rounded-md w-full h-48 mb-10 text-9xl">
-                PROTECT
-            </button>
-            <button class="btn bg-pudar border-none rounded-md w-full h-48 text-9xl my-12">
-                IPW
-            </button>
-        </div>
-        <!-- Protect IPW -->
+    <div class="md:m-24 xs:m-3">
+        <button
+          class="btn bg-pudar border-none rounded-md w-full h-48 md:text-9xl xs:text-6xl"
+        >
+          PROTECT
+        </button>
+        <button
+          class="btn bg-pudar border-none rounded-md w-full h-48 md:text-9xl xs:text-6xl my-12"
+        >
+          IPW
+        </button>
+      </div>
+      <!-- Protect IPW -->
         @include('guest.kajian')
         @include('guest.payment')
         @include('guest.contact')
